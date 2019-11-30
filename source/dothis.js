9809418d3fa3cd227c7e218038072dde4340485a
Alyssa <37455203+thelissbliss@users.noreply.github.com>
2019-11-29 22:33:05 -0800

    mergesort
    mergesort I think it looks right, need to add functions to other file

--- source/dothis.js
+++ source/dothis.js
@@ -10,6 +10,7 @@ var BCounter = 0;
 
 //Merge Sort Variables
 var MSortHolder = [];
+var MCounter = 0;
 
 //Quick Sort Variables
 var QSortHolder = [];
@@ -49,4 +50,54 @@ function Bformat(values){
 		
 		
 		
-		
+
+// mergesort -- in progress
+// Merge Sort Implentation (Recursion)
+function mergeSort (values) {
+  // No need to sort the array if the array only has one element or empty
+  if (values.length <= 1) {
+    return values;
+  }
+  // In order to divide the array in half, we need to figure out the middle
+  const middle = Math.floor(values.length / 2);
+
+  // This is where we will be dividing the array into left and right
+  const left = values.slice(0, middle);
+  const right = values.slice(middle);
+
+  // Using recursion to combine the left and right
+  return merge(
+    mergeSort(left), mergeSort(right)
+  );
+}
+
+// Merge the two arrays: left and right
+function merge (left, right) {
+  let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+  // We will concatenate values into the resultArray in order
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // move left array cursor
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // move right array cursor
+    }
+  }
+
+  // We need to concat here because there will be one element remaining
+  // from either left OR the right
+  return resultArray
+          .concat(left.slice(leftIndex))
+          .concat(right.slice(rightIndex));
+}
+
+//Formats array value into string
+//String is then stored in BSortHolder Array
+function Mformat(resultArray){
+	var converter = resultArray.toString();
+	converter = "[" +converter +"]"
+	MSortHolder[MCounter]=converter;
+	MCounter++;
+}
